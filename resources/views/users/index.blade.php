1f0727eb3a5692e7b9270b93a25bfb56f94aff97
Rama Prayoga <prayogarama12@gmail.com>
2025-05-27 09:30:29 +0700

    last

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,22 @@
-<h1>Daftar User</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Daftar Users') }}
+        </h2>
+    </x-slot>
 
-<a href="{{ route('users.create') }}">+ Tambah User</a>
-
-<ul>
-    @foreach($users as $user)
-        <li>{{ $user->name }} - {{ $user->getRoleNames()->first() }}</li>
-    @endforeach
-</ul>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ route('users.create') }}">+ Tambah User</a>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <ul>
+                        @foreach($users as $user)
+                            <li>{{ $user->name }} - {{ $user->getRoleNames()->first() }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
